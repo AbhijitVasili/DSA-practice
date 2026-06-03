f983aadf7450b9dbbd3c8ae5a97c256eb403bc09
AbhijitVasili <abhijit.vasili@gmail.com>
2026-06-02 21:29:03 -0400

    append LL

---
+++ LinkedLists/append_LL.py
@@ -0,0 +1,22 @@
+class Node(self,value):
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
